89b32de4549ba1ed2921d4e24353e2a1693403d4
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-09-09 21:43:00 -0300

    Changes on objectmodel to xlsx

--- HandleMessageCollector/handleSchedule/xlsxHandler/convertionHandler.js
+++ HandleMessageCollector/handleSchedule/xlsxHandler/convertionHandler.js
@@ -1,4 +1,4 @@
-const xlsx = require('json-as-xlsx');
+const ExcelJS = require('exceljs');
 const fs = require('fs');
 require('dotenv').config();
 const { handleUsers } = require('../scheduledUsers/index');
@@ -6,32 +6,31 @@ const { handleUsers } = require('../scheduledUsers/index');
 const conversionHandler = async (client) => {
 	const user = handleUsers();
 	const columns = [
-		{ label: 'Nome', value: 'name' },
-		{ label: 'Data', value: 'date', format: 'dd-mm-yy' },
-		{ label: 'Profissional', value: 'professional' },
-		{ label: 'Serviço', value: 'service' },
+		{ name: 'Nome', filterButton: true },
+		{ name: 'Data', filterButton: true },
+		{ name: 'Profissional', filterButton: true },
+		{ name: 'Serviços', filterButton: true },
 	];
 	const content = user.map((each) => {
-		return {
-			name: each.name,
-			date: each.date,
-			professional: each.professional,
-			service: each.service,
-		};
+		return [each.name, each.date, each.professional, each.service];
 	});
-	const data = [
-		{
-			sheet: 'Agendaos',
-			columns,
-			content,
+	const workbook = new ExcelJS.Workbook();
+	const sheet = workbook.addWorksheet('Agendados', { views: [{ state: 'frozen', ySplit: 1 }] });
+	workbook.creator = 'Luis Paulo Martins';
+	workbook.created = Date.now().toLocaleString('pt-BR');
+	sheet.addTable({
+		name: 'Agendados',
+		ref: 'A1',
+		headerRow: true,
+		totalsRow: false,
+		style: {
+			theme: 'TableStyleMedium1',
+			showRowStripes: true,
 		},
-	];
-	const settings = {
-		fileName: 'Agendados', // Name of the resulting spreadsheet
-		extraLength: 3, // A bigger number means that columns will be wider
-		writeOptions: {},
-	};
-	xlsx(data, settings);
+		columns,
+		rows: content,
+	});
+	workbook.xlsx.writeFile('D:/WPP/Agendados.xlsx');
 	await client.sendFile(process.env.ATENDENTE, 'D:/WPP/Agendados.xlsx', 'Agendados.xlsx');
 	// eslint-disable-next-line consistent-return
 	fs.unlink('D:/WPP/Agendados.xlsx', (err) => {
