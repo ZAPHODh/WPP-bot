9939e56406025938591e8885e92a423eb0d0d915
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-09-11 09:34:11 -0300

    Send the schedule.xlsx to atendente  working

--- HandleMessageCollector/handleSchedule/xlsxHandler/convertionHandler.js
+++ HandleMessageCollector/handleSchedule/xlsxHandler/convertionHandler.js
@@ -1,42 +1,51 @@
 const ExcelJS = require('exceljs');
 const fs = require('fs');
 require('dotenv').config();
+const path = require('path');
 const { handleUsers } = require('../scheduledUsers/index');
 
 const conversionHandler = async (client) => {
 	const user = handleUsers();
+	console.log(user);
 	const columns = [
 		{ name: 'Nome', filterButton: true },
 		{ name: 'Data', filterButton: true },
 		{ name: 'Profissional', filterButton: true },
 		{ name: 'Serviços', filterButton: true },
 	];
-	const content = user.map((each) => {
+	const rows = user.map((each) => {
 		return [each.name, each.date, each.professional, each.service];
 	});
 	const workbook = new ExcelJS.Workbook();
-	const sheet = workbook.addWorksheet('Agendados', { views: [{ state: 'frozen', ySplit: 1 }] });
+
 	workbook.creator = 'Luis Paulo Martins';
-	workbook.created = Date.now().toLocaleString('pt-BR');
+
+	const sheet = workbook.addWorksheet('Agendados', { views: [{ state: 'frozen', ySplit: 1 }] });
+
 	sheet.addTable({
 		name: 'Agendados',
 		ref: 'A1',
 		headerRow: true,
 		totalsRow: false,
 		style: {
-			theme: 'TableStyleMedium1',
+			theme: 'TableStyleMedium2',
 			showRowStripes: true,
 		},
 		columns,
-		rows: content,
+		rows,
 	});
-	workbook.xlsx.writeFile('D:/WPP/Agendados.xlsx');
-	await client.sendFile(process.env.ATENDENTE, 'D:/WPP/Agendados.xlsx', 'Agendados.xlsx');
-	// eslint-disable-next-line consistent-return
-	fs.unlink('D:/WPP/Agendados.xlsx', (err) => {
-		if (err) {
-			return err;
-		}
-	});
+	const file = 'Agendados.xlsx';
+	workbook.xlsx.writeFile(path.join(__dirname, file));
+	try {
+		await client.sendFile(
+			process.env.ATENDENTE,
+			path.join(__dirname, file),
+			'Aqui está a planilha  de agendamento atualizada.',
+		);
+		fs.unlink(file);
+	} catch (err) {
+		console.log(err);
+		fs.unlink(file);
+	}
 };
 module.exports = conversionHandler;
